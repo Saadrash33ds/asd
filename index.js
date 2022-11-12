8909161e50d07050b0d53b4c7a2102b6a96c9ce8
Muhammad Saad <Saadrash33d94@gmail.com>
2022-11-12 18:59:55 +0500

    aaa

---
+++ index.js
@@ -0,0 +1,106 @@
+// chapter 1:Alerts
+
+// alert("Boo!");
+// alert("You Won!");
+// alert("Hi!");
+
+
+// chapter 2: Variables for strings
+
+var bestman="charlie";
+var bestman="Tom";
+
+console.log(bestman)
+
+var nam;
+nam="Tom";
+console.log(nam)
+var city
+city="Boston"
+console.log(city)
+var friend="Talha"
+friend="No One"
+console.log(friend)
+
+// Chapter 3: Variables for Numbers
+
+var weight=150;
+
+
+console.log(weight+25)
+
+var num1=23
+var num2=7
+var num3=num1 + num2;
+
+console.log(num3)
+
+var num4="23"
+var newnum= num4+7;
+
+console.log(newnum)
+
+
+var city1st = "Karachi";
+
+console.log(city1st)
+
+
+
+//chapter5: Math ex pressions: Familiar operators
+
+var popnum=4+4;
+
+// alert(2+2)
+
+var x=10
+var y=10
+var z= x*y
+console.log(z)
+
+
+var remainder=10%3;
+
+console.log( "The remainder of 10 is=",remainder)
+
+//chapter6: Math ex pressions: UnFamiliar operators
+
+
+var num=2
+var newnum=num++;
+
+console.log(newnum)
+
+console.log(num)
+
+
+// chapter 7:Eliminating ambiguity
+
+num1=1+4*7;
+
+console.log(num1)
+
+var Res=(2 * 4) * 4 + 2;
+var Res1=(2 * 4) * (4 + 2);
+console.log(Res)
+console.log(Res1)
+
+// chapter 8: Concatenating text strings
+
+var msg1="I"
+var msg2=" Love"
+var msg3=" You"
+
+console.log(msg1 +msg2 +msg3)
+
+console.log("2 plus 2 equals",2+2)
+
+var msg="Great"
+
+console.log("You Are "+msg+" Sir Basit")
+
+
+
+
+
+
